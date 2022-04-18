36e6f113057d10f3ba4a4cef760bc7caf8616bee
ich777 <christophhummer@gmail.com>
2022-04-18 14:45:20 +0200

    „scripts/start.sh“ ändern

--- scripts/start.sh
+++ scripts/start.sh
@@ -19,11 +19,12 @@ else
     echo "---No optional script found, continuing---"
 fi
 
-echo "---Starting...---"
+echo "---Taking ownership of data...---"
 chown -R root:${GID} /opt/scripts
 chmod -R 750 /opt/scripts
 chown -R ${UID}:${GID} ${DATA_DIR}
 
+echo "---Starting...---"
 term_handler() {
 	kill -SIGINT $(pgrep TheForestDedica)
 	tail --pid=$(pgrep TheForestDedica) -f 2>/dev/null
