6b9d703a8605f2896e4e610bb5115211874cb50c
ich777 <christophhummer@gmail.com>
2022-05-17 19:17:47 +0200

    „scripts/start.sh“ ändern

--- scripts/start.sh
+++ scripts/start.sh
@@ -26,8 +26,8 @@ chown -R ${UID}:${GID} ${DATA_DIR}
 
 echo "---Starting...---"
 term_handler() {
-	kill -SIGTERM "$killpid"
-	wait "$killpid" -f 2>/dev/null
+	kill -SIGINT $(pidof VRisingServer.exe)
+	tail --pid=$(pidof VRisingServer.exe) -f 2>/dev/null
 	exit 143;
 }
 
