f24da8fe35452167139ac80e584874e6ce181a93
ich777 <christophhummer@gmail.com>
2022-05-18 17:37:51 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -86,4 +86,4 @@ cd ${SERVER_DIR}
 xvfb-run --auto-servernum --server-args='-screen 0 640x480x24:32' wine64 ${SERVER_DIR}/VRisingServer.exe -persistentDataPath ${SERVER_DIR}/save-data -serverName "${SERVER_NAME}" -saveName "${WORLD_NAME}" -logFile ${SERVER_DIR}/logs/VRisingServer.log ${GAME_PARAMS} >/dev/null 2>&1 &
 sleep 5
 /opt/scripts/start-watchdog.sh &
-tail -f ${SERVER_DIR}/logs/VRisingServer.log
+tail -n +0 -f ${SERVER_DIR}/logs/VRisingServer.log
