244cca033b4acf96cda77315f8c9d5b5c483627f
ich777 <christophhummer@gmail.com>
2022-06-26 21:37:10 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -69,4 +69,7 @@ echo "---Server ready---"
 
 echo "---Start Server---"
 cd ${SERVER_DIR}
-${SERVER_DIR}/rocketstation_DedicatedServer.x86_64 -autostart -batchmode -nographics ${GAME_PARAMS}
+screen -S Stationeers -L -Logfile $SERVER_DIR/masterLog.0 -d -m ${SERVER_DIR}/rocketstation_DedicatedServer.x86_64 -autostart -batchmode -nographics ${GAME_PARAMS}
+sleep 2
+/opt/scripts/start-watchdog.sh &
+tail -f ${SERVER_DIR}/masterLog.0
