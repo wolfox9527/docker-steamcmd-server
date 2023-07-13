05f20eac1e392ffd1e905a6dc75fd00421643db2
ich777 <christophhummer@gmail.com>
2023-07-13 08:09:00 +0200

    Update scripts/start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -65,4 +65,4 @@ echo "---Server ready---"
 
 echo "---Start Server---"
 cd ${SERVER_DIR}
-${SERVER_DIR}/srcds_run -game ${GAME_NAME} ${GAME_PARAMS} -console +port ${GAME_PORT}
+${SERVER_DIR}/srcds_run -game ${GAME_NAME} ${GAME_PARAMS} -console +port -nographics -batchmode ${GAME_PORT}
