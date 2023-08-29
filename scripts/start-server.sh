4b6613b811daed27f321a851b8eab9d09eed4531
ich777 <christophhummer@gmail.com>
2023-08-29 19:04:24 +0200

    scripts/start-server.sh aktualisiert

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -57,6 +57,7 @@ fi
 echo "---Prepare Server---"
 export WINEARCH=win64
 export WINEPREFIX=/serverdata/serverfiles/WINE64
+export DISPLAY=:99
 echo "---Checking if WINE workdirectory is present---"
 if [ ! -d ${SERVER_DIR}/WINE64 ]; then
 	echo "---WINE workdirectory not found, creating please wait...---"
@@ -74,6 +75,33 @@ else
 	echo "---WINE properly set up---"
 fi
 
+echo "---Checking if database is in place---"
+if [ ! -d ${SERVER_DIR}/.database ]; then
+  echo "---Database not found, setting up databbase...---"
+  mkdir -p ${SERVER_DIR}/.database
+  cp -R /var/lib/mysql/* ${SERVER_DIR}/.database/
+else
+  echo "---Database found---"
+fi
+
+echo "---Checking if database configuration is in place---"
+if [ ! -f ${SERVER_DIR}/config_local.cs ]; then
+  echo "---Database configuration not found, configuring...---"
+  cp ${SERVER_DIR}/docs/config_local.cs ${SERVER_DIR}/config_local.cs
+  sed -i 's/\<root\>/steam/g' ${SERVER_DIR}/config_local.cs
+  sed -i 's/\<rootPassword\>/lifyo/g' ${SERVER_DIR}/config_local.cs
+else
+  echo "---Database connection found---"
+fi
+
+echo "---Starting MariaDB---"
+screen -S MariaDB -d -m mysqld_safe
+sleep 2
+
+echo "---Starting Xvfb---"
+screen -S Xvfb -d -m /opt/scripts/start-Xvfb.sh
+sleep 2
+
 echo "---Checking for old display lock files---"
 find /tmp -name ".X99*" -exec rm -f {} \; > /dev/null 2>&1
 chmod -R ${DATA_PERM} ${DATA_DIR}
@@ -82,10 +110,16 @@ echo "---Server ready---"
 sleep infinity
 
 echo "---Start Server---"
-if [ ! -f ${SERVER_DIR}/server64.exe ]; then
+if [ ! -f ${SERVER_DIR}/ddctd_cm_yo_server.exe ]; then
   echo "---Something went wrong, can't find the executable, putting container into sleep mode!---"
   sleep infinity
 else
   cd ${SERVER_DIR}
-  wine64 server64.exe ${GAME_PARAMS}
+  screen -S LiFYO -d -m wine64 ddctd_cm_yo_server.exe ${GAME_PARAMS}
+  echo "---Waiting for logs...---"
+  sleep 3
+  /opt/scripts/start-watchdog.sh &
+  cd ${SERVER_DIR}/logs
+  cd $(ls -1d */ | tail -1)
+  tail -n 9999 -f $(ls -t | head -n 1)
 fi
