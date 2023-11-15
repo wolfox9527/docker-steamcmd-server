50b9b926968e828817f0acb99a34fda230fbe7c8
ich777 <christophhummer@gmail.com>
2023-11-15 09:50:34 +0100

    scripts/start-server.sh aktualisiert

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,72 +1,109 @@
 #!/bin/bash
-echo "---This container is under construction!---"
-echo "...Sleep zZzZzZzZzZzZZzzZzZZZzZZzZzZzZzz..."
-sleep infinity
-
 if [ ! -f ${STEAMCMD_DIR}/steamcmd.sh ]; then
-    echo "SteamCMD not found!"
-    wget -q -O ${STEAMCMD_DIR}/steamcmd_linux.tar.gz http://media.steampowered.com/client/steamcmd_linux.tar.gz 
-    tar --directory ${STEAMCMD_DIR} -xvzf /serverdata/steamcmd/steamcmd_linux.tar.gz
-    rm ${STEAMCMD_DIR}/steamcmd_linux.tar.gz
+  echo "SteamCMD not found!"
+  wget -q -O ${STEAMCMD_DIR}/steamcmd_linux.tar.gz http://media.steampowered.com/client/steamcmd_linux.tar.gz 
+  tar --directory ${STEAMCMD_DIR} -xvzf /serverdata/steamcmd/steamcmd_linux.tar.gz
+  rm ${STEAMCMD_DIR}/steamcmd_linux.tar.gz
 fi
 
 echo "---Update SteamCMD---"
 if [ "${USERNAME}" == "" ]; then
-    ${STEAMCMD_DIR}/steamcmd.sh \
-    +login anonymous \
-    +quit
+  ${STEAMCMD_DIR}/steamcmd.sh \
+  +login anonymous \
+  +quit
 else
-    ${STEAMCMD_DIR}/steamcmd.sh \
-    +login ${USERNAME} ${PASSWRD} \
-    +quit
+  ${STEAMCMD_DIR}/steamcmd.sh \
+  +login ${USERNAME} ${PASSWRD} \
+  +quit
 fi
 
 echo "---Update Server---"
 if [ "${USERNAME}" == "" ]; then
-    if [ "${VALIDATE}" == "true" ]; then
-    	echo "---Validating installation---"
-        ${STEAMCMD_DIR}/steamcmd.sh \
-        +force_install_dir ${SERVER_DIR} \
-        +login anonymous \
-        +app_update ${GAME_ID} validate \
-        +quit
-    else
-        ${STEAMCMD_DIR}/steamcmd.sh \
-        +force_install_dir ${SERVER_DIR} \
-        +login anonymous \
-        +app_update ${GAME_ID} \
-        +quit
-    fi
+  if [ "${VALIDATE}" == "true" ]; then
+    echo "---Validating installation---"
+    ${STEAMCMD_DIR}/steamcmd.sh \
+    +@sSteamCmdForcePlatformType windows \
+    +force_install_dir ${SERVER_DIR} \
+    +login anonymous \
+    +app_update ${GAME_ID} validate \
+    +quit
+  else
+    ${STEAMCMD_DIR}/steamcmd.sh \
+    +@sSteamCmdForcePlatformType windows \
+    +force_install_dir ${SERVER_DIR} \
+    +login anonymous \
+    +app_update ${GAME_ID} \
+    +quit
+  fi
 else
-    if [ "${VALIDATE}" == "true" ]; then
-    	echo "---Validating installation---"
-        ${STEAMCMD_DIR}/steamcmd.sh \
-        +force_install_dir ${SERVER_DIR} \
-        +login ${USERNAME} ${PASSWRD} \
-        +app_update ${GAME_ID} validate \
-        +quit
-    else
-        ${STEAMCMD_DIR}/steamcmd.sh \
-        +force_install_dir ${SERVER_DIR} \
-        +login ${USERNAME} ${PASSWRD} \
-        +app_update ${GAME_ID} \
-        +quit
-    fi
+  if [ "${VALIDATE}" == "true" ]; then
+    echo "---Validating installation---"
+    ${STEAMCMD_DIR}/steamcmd.sh \
+    +@sSteamCmdForcePlatformType windows \
+    +force_install_dir ${SERVER_DIR} \
+    +login ${USERNAME} ${PASSWRD} \
+    +app_update ${GAME_ID} validate \
+    +quit
+  else
+    ${STEAMCMD_DIR}/steamcmd.sh \
+    +@sSteamCmdForcePlatformType windows \
+    +force_install_dir ${SERVER_DIR} \
+    +login ${USERNAME} ${PASSWRD} \
+    +app_update ${GAME_ID} \
+    +quit
+  fi
 fi
 
-echo "---Prepare Server---"
-if [ ! -f ${DATA_DIR}/.steam/sdk32/steamclient.so ]; then
-	if [ ! -d ${DATA_DIR}/.steam ]; then
-    	mkdir ${DATA_DIR}/.steam
-    fi
-	if [ ! -d ${DATA_DIR}/.steam/sdk32 ]; then
-    	mkdir ${DATA_DIR}/.steam/sdk32
-    fi
-    cp -R ${STEAMCMD_DIR}/linux32/* ${DATA_DIR}/.steam/sdk32/
+echo "---Checking the maximum map count per process...---"
+CUR_MAX_MAP_COUNT=$(cat /proc/sys/vm/max_map_count)
+if [[ $CUR_MAX_MAP_COUNT -ge 256000 ]]; then
+  echo "---Maximum map count per process OK...---"
+  echo "---Current map count per process: $CUR_MAX_MAP_COUNT---"
+else
+  echo
+  echo "+---ATTENTION---ATTENTION---ATTENTION---ATTENTION---ATTENTION---"
+  echo "| Maximum map count per process too low, currently: $CUR_MAX_MAP_COUNT"
+  echo "| Please set the value to at least '256000' on the host and"
+  echo "| restart the container afterwards."
+  echo "|"
+  echo "| You can change the value by executing this command on the host"
+  echo "| as root:"
+  echo
+  echo "echo 265000 > /proc/sys/vm/max_map_count"
+  echo
+  echo "| You can make that persistent by using a User Script that runs"
+  echo "| on startup or putting this line in your go file."
+  echo "+---ATTENTION---ATTENTION---ATTENTION---ATTENTION---ATTENTION---"
+  echo
+  echo "---Putting container into sleep mode!---"
+  sleep infinity
 fi
+
+export WINEARCH=win64
+export WINEPREFIX=/serverdata/serverfiles/WINE64
+export WINEDEBUG=-all
+echo "---Checking if WINE workdirectory is present---"
+if [ ! -d ${SERVER_DIR}/WINE64 ]; then
+  echo "---WINE workdirectory not found, creating please wait...---"
+  mkdir ${SERVER_DIR}/WINE64
+else
+  echo "---WINE workdirectory found---"
+fi
+echo "---Checking if WINE is properly installed---"
+if [ ! -d ${SERVER_DIR}/WINE64/drive_c/windows ]; then
+  echo "---Setting up WINE---"
+  cd ${SERVER_DIR}
+  winecfg > /dev/null 2>&1
+  sleep 15
+else
+  echo "---WINE properly set up---"
+fi
+echo "---Prepare Server---"
 chmod -R ${DATA_PERM} ${DATA_DIR}
 echo "---Server ready---"
 
 echo "---Start Server---"
-cd ${SERVER_DIR}
-${SERVER_DIR}/srcds_run -game ${GAME_NAME} ${GAME_PARAMS} -console +port ${GAME_PORT}
+cd ${SERVER_DIR}/ShooterGame/Binaries/Win64
+wine64 ArkAscendedServer.exe ${MAP}?listen?SessionName=${SERVER_NAME}?ServerPassword=${SRV_PWD}?ServerAdminPassword=${SRV_ADMIN_PWD}${GAME_PARAMS} ${GAME_PARAMS_EXTRA}
+sleep 1
+tail -f ${SERVER_DIR}/ShooterGame/Saved/Logs/ShooterGame.log
