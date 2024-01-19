f1455aabdbb814bd297b88b30f9753a46f9f203c
ich777 <christophhummer@gmail.com>
2024-01-19 10:05:49 +0100

    scripts/start-server.sh aktualisiert

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -22,14 +22,12 @@ if [ "${USERNAME}" == "" ]; then
   if [ "${VALIDATE}" == "true" ]; then
     echo "---Validating installation---"
     ${STEAMCMD_DIR}/steamcmd.sh \
-    +@sSteamCmdForcePlatformType windows \
     +force_install_dir ${SERVER_DIR} \
     +login anonymous \
     +app_update ${GAME_ID} validate \
     +quit
   else
     ${STEAMCMD_DIR}/steamcmd.sh \
-    +@sSteamCmdForcePlatformType windows \
     +force_install_dir ${SERVER_DIR} \
     +login anonymous \
     +app_update ${GAME_ID} \
@@ -39,14 +37,12 @@ else
   if [ "${VALIDATE}" == "true" ]; then
     echo "---Validating installation---"
     ${STEAMCMD_DIR}/steamcmd.sh \
-    +@sSteamCmdForcePlatformType windows \
     +force_install_dir ${SERVER_DIR} \
     +login ${USERNAME} ${PASSWRD} \
     +app_update ${GAME_ID} validate \
     +quit
   else
     ${STEAMCMD_DIR}/steamcmd.sh \
-    +@sSteamCmdForcePlatformType windows \
     +force_install_dir ${SERVER_DIR} \
     +login ${USERNAME} ${PASSWRD} \
     +app_update ${GAME_ID} \
@@ -54,50 +50,9 @@ else
   fi
 fi
 
-echo "---Checking the maximum map count per process...---"
-CUR_MAX_MAP_COUNT=$(cat /proc/sys/vm/max_map_count)
-if [[ $CUR_MAX_MAP_COUNT -ge 256000 ]]; then
-  echo "---Maximum map count per process OK...---"
-  echo "---Current map count per process: $CUR_MAX_MAP_COUNT---"
-else
-  echo
-  echo "+---ATTENTION---ATTENTION---ATTENTION---ATTENTION---ATTENTION---"
-  echo "| Maximum map count per process too low, currently: $CUR_MAX_MAP_COUNT"
-  echo "| Please set the value to at least '256000' on the host and"
-  echo "| restart the container afterwards."
-  echo "|"
-  echo "| You can change the value by executing this command on the host"
-  echo "| as root:"
-  echo
-  echo "echo 265000 > /proc/sys/vm/max_map_count"
-  echo
-  echo "| You can make that persistent by using a User Script that runs"
-  echo "| on startup or putting this line in your go file."
-  echo "+---ATTENTION---ATTENTION---ATTENTION---ATTENTION---ATTENTION---"
-  echo
-  echo "---Putting container into sleep mode!---"
-  sleep infinity
-fi
+echo "Under construction, sleep zZzZzZzZz..."
+sleep infinity
 
-export WINEARCH=win64
-export WINEPREFIX=/serverdata/serverfiles/WINE64
-export WINEDEBUG=-all
-echo "---Checking if WINE workdirectory is present---"
-if [ ! -d ${SERVER_DIR}/WINE64 ]; then
-  echo "---WINE workdirectory not found, creating please wait...---"
-  mkdir ${SERVER_DIR}/WINE64
-else
-  echo "---WINE workdirectory found---"
-fi
-echo "---Checking if WINE is properly installed---"
-if [ ! -d ${SERVER_DIR}/WINE64/drive_c/windows ]; then
-  echo "---Setting up WINE---"
-  cd ${SERVER_DIR}
-  winecfg > /dev/null 2>&1
-  sleep 15
-else
-  echo "---WINE properly set up---"
-fi
 echo "---Prepare Server---"
 chmod -R ${DATA_PERM} ${DATA_DIR}
 echo "---Server ready---"
