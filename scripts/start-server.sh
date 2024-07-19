c086dbbc20bb94fcb743de887b2d3f489a9aa9c3
Christoph Hummer <christophhummer@gmail.com>
2024-07-19 12:59:49 +0200

    scripts/start-server.sh aktualisiert

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -155,6 +155,6 @@ echo "---Server ready---"
 echo "---Start Server---"
 cd ${SERVER_DIR}
 xvfb-run --auto-servernum --server-args='-screen 0 640x480x24:32' wine64 ${SERVER_DIR}/ConanSandboxServer.exe -log ${GAME_PARAMS} >/dev/null 2>&1 &
-sleep 5
+sleep 10
 /opt/scripts/start-watchdog.sh &
 tail -n +0 -f ${SERVER_DIR}/ConanSandbox/Saved/Logs/ConanSandbox.log
