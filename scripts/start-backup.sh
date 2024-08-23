bf20c62986b2cfcac49eb61a882a3620a63d5434
Christoph Hummer <christophhummer@gmail.com>
2024-08-23 18:14:27 +0200

    scripts/start-backup.sh aktualisiert

--- scripts/start-backup.sh
+++ scripts/start-backup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 while true
 do
-	sleep ${BACKUP_INTERVAL}m 
-	cd ${SERVER_DIR}/Saves
-	tar -czf ${SERVER_DIR}/Backups/$(date '+%Y-%m-%d_%H.%M.%S').tar.gz .
+	sleep ${BACKUP_INTERVAL}m
+	if [ -d ${SERVER_DIR}/User/Saves ]; then
+		cd ${SERVER_DIR}/User/Saves
+		tar -czf ${SERVER_DIR}/Backups/$(date '+%Y-%m-%d_%H.%M.%S').tar.gz .
+	else
+		echo "Backup ERROR: Save path ${SERVER_DIR}/User/Saves not found!"
+	fi
 	cd ${SERVER_DIR}/Backups
 	ls -1tr ${SERVER_DIR}/Backups | sort | head -n -${BACKUP_TO_KEEP} | xargs -d '\n' rm -f --
 	chmod -R ${DATA_PERM} ${SERVER_DIR}/Backups
