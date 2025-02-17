5c3092af13ee8b103e379ca476b17b523030cc73
Christoph Hummer <christophhummer@gmail.com>
2025-02-17 19:41:16 +0100

    scripts/start-server.sh aktualisiert

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,141 +1,97 @@
 #!/bin/bash
-export DISPLAY=:99
 if [ ! -f ${STEAMCMD_DIR}/steamcmd.sh ]; then
-    echo "SteamCMD not found!"
-    wget -q -O ${STEAMCMD_DIR}/steamcmd_linux.tar.gz http://media.steampowered.com/client/steamcmd_linux.tar.gz 
-    tar --directory ${STEAMCMD_DIR} -xvzf /serverdata/steamcmd/steamcmd_linux.tar.gz
-    rm ${STEAMCMD_DIR}/steamcmd_linux.tar.gz
+  echo "SteamCMD not found!"
+  wget -q -O ${STEAMCMD_DIR}/steamcmd_linux.tar.gz http://media.steampowered.com/client/steamcmd_linux.tar.gz 
+  tar --directory ${STEAMCMD_DIR} -xvzf /serverdata/steamcmd/steamcmd_linux.tar.gz
+  rm ${STEAMCMD_DIR}/steamcmd_linux.tar.gz
 fi
 
 echo "---Update SteamCMD---"
 if [ "${USERNAME}" == "" ]; then
-    ${STEAMCMD_DIR}/steamcmd.sh \
-    +login anonymous \
-    +quit
+  ${STEAMCMD_DIR}/steamcmd.sh \
+  +login anonymous \
+  +quit
 else
-    ${STEAMCMD_DIR}/steamcmd.sh \
-    +login ${USERNAME} ${PASSWRD} \
-    +quit
+  ${STEAMCMD_DIR}/steamcmd.sh \
+  +login ${USERNAME} ${PASSWRD} \
+  +quit
 fi
 
 echo "---Update Server---"
 if [ "${USERNAME}" == "" ]; then
-    if [ "${VALIDATE}" == "true" ]; then
-    	echo "---Validating installation---"
-        ${STEAMCMD_DIR}/steamcmd.sh \
-        +@sSteamCmdForcePlatformType windows \
-        +force_install_dir ${SERVER_DIR} \
-        +login anonymous \
-        +app_update ${GAME_ID} validate \
-        +quit
-    else
-        ${STEAMCMD_DIR}/steamcmd.sh \
-        +@sSteamCmdForcePlatformType windows \
-        +force_install_dir ${SERVER_DIR} \
-        +login anonymous \
-        +app_update ${GAME_ID} \
-        +quit
-    fi
+  if [ "${VALIDATE}" == "true" ]; then
+    echo "---Validating installation---"
+    ${STEAMCMD_DIR}/steamcmd.sh \
+    +@sSteamCmdForcePlatformType windows \
+    +force_install_dir ${SERVER_DIR} \
+    +login anonymous \
+    +app_update ${GAME_ID} validate \
+    +quit
+  else
+    ${STEAMCMD_DIR}/steamcmd.sh \
+    +@sSteamCmdForcePlatformType windows \
+    +force_install_dir ${SERVER_DIR} \
+    +login anonymous \
+    +app_update ${GAME_ID} \
+    +quit
+  fi
 else
-    if [ "${VALIDATE}" == "true" ]; then
-    	echo "---Validating installation---"
-        ${STEAMCMD_DIR}/steamcmd.sh \
-        +@sSteamCmdForcePlatformType windows \
-        +force_install_dir ${SERVER_DIR} \
-        +login ${USERNAME} ${PASSWRD} \
-        +app_update ${GAME_ID} validate \
-        +quit
-    else
-        ${STEAMCMD_DIR}/steamcmd.sh \
-        +@sSteamCmdForcePlatformType windows \
-        +force_install_dir ${SERVER_DIR} \
-        +login ${USERNAME} ${PASSWRD} \
-        +app_update ${GAME_ID} \
-        +quit
-    fi
+  if [ "${VALIDATE}" == "true" ]; then
+    echo "---Validating installation---"
+    ${STEAMCMD_DIR}/steamcmd.sh \
+    +@sSteamCmdForcePlatformType windows \
+    +force_install_dir ${SERVER_DIR} \
+    +login ${USERNAME} ${PASSWRD} \
+    +app_update ${GAME_ID} validate \
+    +quit
+  else
+    ${STEAMCMD_DIR}/steamcmd.sh \
+    +@sSteamCmdForcePlatformType windows \
+    +force_install_dir ${SERVER_DIR} \
+    +login ${USERNAME} ${PASSWRD} \
+    +app_update ${GAME_ID} \
+    +quit
+  fi
 fi
 
-echo "---Prepare Server---"
 export WINEARCH=win64
 export WINEPREFIX=/serverdata/serverfiles/WINE64
+export WINEDEBUG=-all
 echo "---Checking if WINE workdirectory is present---"
 if [ ! -d ${SERVER_DIR}/WINE64 ]; then
-	echo "---WINE workdirectory not found, creating please wait...---"
-    mkdir ${SERVER_DIR}/WINE64
+  echo "---WINE workdirectory not found, creating please wait...---"
+  mkdir ${SERVER_DIR}/WINE64
 else
-	echo "---WINE workdirectory found---"
+  echo "---WINE workdirectory found---"
 fi
 echo "---Checking if WINE is properly installed---"
 if [ ! -d ${SERVER_DIR}/WINE64/drive_c/windows ]; then
-	echo "---Setting up WINE---"
-    cd ${SERVER_DIR}
-    winecfg > /dev/null 2>&1
-    sleep 15
-    wineserver -k >/dev/null 2>&1
+  echo "---Setting up WINE---"
+  cd ${SERVER_DIR}
+  winecfg > /dev/null 2>&1
+  sleep 15
 else
-	echo "---WINE properly set up---"
-fi
-
-if [ -f ${SERVER_DIR}/runtimes ]; then
-  rm -f ${SERVER_DIR}/runtimes
-  touch ${SERVER_DIR}/WINE64/runtimes
-fi
-
-echo "---Checking if runtimes are installed---"
-if [ ! -f ${SERVER_DIR}/WINE64/runtimes ]; then
-  echo "---Runtimes not installed, please wait installing...---"
-  mkdir -p ${DATA_DIR}/.cache/winetricks
-  find /tmp -name ".X99*" -exec rm -f {} \; > /dev/null 2>&1
-  /opt/scripts/start-Xvfb.sh 2>/dev/null &
-  echo "---...this can take some time...---"
-  sleep 5
-  timeout 10m /usr/bin/winetricks -q dotnet45 2>/dev/null
-  timeout 3m /usr/bin/winetricks -q vcrun2019 2>/dev/null
-  wine64 ${SERVER_DIR}/IcarusServer.exe -log ${GAME_PARAMS} >/dev/null 2&>1 &
-  sleep 10
-  wineserver -k >/dev/null 2>&1
-  kill $(pidof Xvfb) 2>/dev/null
-  touch ${SERVER_DIR}/WINE64/runtimes
-  echo "---Installation from runtimes finished!---"
-else
-  echo "---Runtimes found! Continuing...---"
-fi
-
-echo "---Looking 'ServerSettings.ini' file is in place---"
-if [ ! -f ${SERVER_DIR}/Icarus/Saved/Config/WindowsServer/ServerSettings.ini ]; then
-  echo "---'ServerSettings.ini' not found, copying template...---"
-  if [ ! -d ${SERVER_DIR}/Icarus/Saved/Config/WindowsServer ]; then
-    mkdir -p ${SERVER_DIR}/Icarus/Saved/Config/WindowsServer
-  fi
-  cp /opt/ServerSettings.ini ${SERVER_DIR}/Icarus/Saved/Config/WindowsServer/
-else
-  echo "---'ServerSettings.ini' found---"
+  echo "---WINE properly set up---"
 fi
 echo "---Checking for old display lock files---"
 find /tmp -name ".X99*" -exec rm -f {} \; > /dev/null 2>&1
 chmod -R ${DATA_PERM} ${DATA_DIR}
 
-echo "---Starting Xvfb server---"
-screen -S Xvfb -d -m /opt/scripts/start-Xvfb.sh
-sleep 5
+echo "---Looking 'MoriaServerConfig.ini' file is in place---"
+if [ ! -f ${SERVER_DIR}/MoriaServerConfig.ini ]; then
+  echo "---'MoriaServerConfig.ini' not found, copying template...---"
+  cp /opt/MoriaServerConfig.ini ${SERVER_DIR}/MoriaServerConfig.ini
+else
+  echo "---'MoriaServerConfig.ini' found---"
+fi
 
-chmod -R ${DATA_PERM} ${DATA_DIR}
 echo "---Server ready---"
 
 echo "---Start Server---"
 cd ${SERVER_DIR}
-if [ ! -f ${SERVER_DIR}/IcarusServer.exe ]; then
+if [ ! -f ${SERVER_DIR}/Moria/Binaries/Win64/MoriaServer-Win64-Shipping.exe ]; then
   echo "---Something went wrong, can't find the executable, putting container into sleep mode!---"
   sleep infinity
 else
-  screen -S Icarus -d -m wine64 ${SERVER_DIR}/IcarusServer.exe -log ${GAME_PARAMS}
-  if [ ! -f ${SERVER_DIR}/Icarus/Saved/Logs/Icarus.log ]; then
-    if [ ! -d ${SERVER_DIR}/Icarus/Saved/Logs ]; then
-      mkdir -p ${SERVER_DIR}/Icarus/Saved/Logs
-    fi
-    touch ${SERVER_DIR}/Icarus/Saved/Logs/Icarus.log
-  fi
-  sleep 2
-  /opt/scripts/start-watchdog.sh &
-  tail -n 9999 -f ${SERVER_DIR}/Icarus/Saved/Logs/Icarus.log
+  xvfb-run --auto-servernum --server-args='-screen 0 640x480x24:32' wine64 ${SERVER_DIR}/Moria/Binaries/Win64/MoriaServer-Win64-Shipping.exe -log ${GAME_PARAMS}
 fi
